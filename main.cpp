b29bd1ccf2862ea08897f64bb59fa9700dc342d3
AVasudevan24 <88952421+AVasudevan24@users.noreply.github.com>
2021-08-14 19:30:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,19 +7,52 @@ MeDCMotor left(M1);
 MeDCMotor right(M2);
 
 void setup() { 
-    // Nothing to setup here!
+    
 }
 
-/* NOTE:
-  lineFinder sensor returns [0] when over BLACK surface
-  lineFinder sensor returns [1] when over WHITE surface
-*/
+
 
 void loop() {
 
-    //////////////////////////////////////////////////////////////////////////////////
-    // STEP 1: Follow straight line until the end and stop
-    //////////////////////////////////////////////////////////////////////////////////
+  
+    // Drive straight following the line
+    while(lineFinder.readSensor1()==0||lineFinder.readSensor2()==0){
+        if(lineFinder.readSensor1()==1){
+            left.run(-110);
+            right.run(50);
+        }
+        else if(lineFinder.readSensor2()==1){
+            left.run(-50);
+            right.run(110);
+        }
+        else{
+            left.run(-70);
+            right.run(70);
+        }
+    }
+
+    // Stop the robot at the end of the line
+    left.stop();
+    right.stop();
+
+    delay(1000);
+
+    
+
+   
+    while (lineFinder.readSensor1()==1){
+        // Rotates to the right
+        left.run(-80);
+        right.run(-80);
+    }
+
+    // Stop the robot
+    left.stop();
+    right.stop();
+
+    delay(1000);
+
+    
 
     // Drive straight following the line
     while(lineFinder.readSensor1()==0||lineFinder.readSensor2()==0){
@@ -43,16 +76,7 @@ void loop() {
 
     delay(1000);
 
-    //////////////////////////////////////////////////////////////////////////////////
-
-
-
-    //////////////////////////////////////////////////////////////////////////////////
-    // STEP 2: Turn in place until the robot senses the vertical line
-    //////////////////////////////////////////////////////////////////////////////////
-
-    // NOTE: The (lineFinder.readSensor1()==1) is used to check that the robot rotates sufficiently
-    // to align with the black tape.
+   
     while (lineFinder.readSensor1()==1){
         // Rotates to the right
         left.run(-80);
@@ -65,65 +89,7 @@ void loop() {
 
     delay(1000);
 
-    //////////////////////////////////////////////////////////////////////////////////
-
-
-
-    //////////////////////////////////////////////////////////////////////////////////
-    // STEP 3: Follow the vertical line until it reaches the gap
-    //////////////////////////////////////////////////////////////////////////////////
-
-    // Drive straight following the line
-    while(lineFinder.readSensor1()==0||lineFinder.readSensor2()==0){
-        if(lineFinder.readSensor1()==1){
-            left.run(-110);
-            right.run(50);
-        }
-        else if(lineFinder.readSensor2()==1){
-            left.run(-50);
-            right.run(110);
-        }
-        else{
-            left.run(-70);
-            right.run(70);
-        }
-    }
-
-    // Stop the robot at the end of the line
-    left.stop();
-    right.stop();
-
-    delay(1000);
-
-    //////////////////////////////////////////////////////////////////////////////////
-
-
-    //////////////////////////////////////////////////////////////////////////////////
-    // STEP 4: Rotate to the right parking spot until it picks up the horizontal 
-    // parking line
-    //////////////////////////////////////////////////////////////////////////////////
-
-    // NOTE: The (lineFinder.readSensor1()==1) is used to check that the robot rotates sufficiently
-    // to align with the black tape.
-    while (lineFinder.readSensor1()==1){
-        // Rotates to the right
-        left.run(-80);
-        right.run(-80);
-    }
-
-    // Stop the robot
-    left.stop();
-    right.stop();
-
-    delay(1000);
-
-    //////////////////////////////////////////////////////////////////////////////////
-
-
-    //////////////////////////////////////////////////////////////////////////////////
-    // STEP 5: Check the RIGHT car park for space, park if its empty, else rotate to
-    // left car park
-    //////////////////////////////////////////////////////////////////////////////////
+ 
 
     if (ultra.distanceCm() > 20.0) {
         // If the space is EMPTY, the mBot parks in the space
@@ -162,14 +128,7 @@ void loop() {
 
     delay(1000);
 
-    //////////////////////////////////////////////////////////////////////////////////
-
-
-
-    //////////////////////////////////////////////////////////////////////////////////
-    // STEP 6: Check the LEFT car park for space, park if its empty, else rotate to
-    // the middle and stop
-    //////////////////////////////////////////////////////////////////////////////////
+  
 
     if (ultra.distanceCm() > 20.0) {
         // If the space is EMPTY, the mBot parks in the space
@@ -201,7 +160,7 @@ void loop() {
     left.stop();
     right.stop();
 
-    //////////////////////////////////////////////////////////////////////////////////
+    
 
     // Park here forever!
     while(true){
@@ -209,11 +168,5 @@ void loop() {
         right.stop();
     }
 
-    //////////////////////////////////////////////////////////////////////////////////
-    // YOUR TURN!
-    //
-    // You can add to this code to check the two parking spaces ahead in case the first two
-    // are occupied.
-    //////////////////////////////////////////////////////////////////////////////////
-
+  
 }
